c2c64842b825aac3b45cae79445978abf772cfdf
bkawk <email.will.in.china@gmail.com>
2019-09-02 23:42:39 +0800

    modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -12,19 +12,23 @@ const Modal: React.FC<propsInterface> = (props) => {
   let modalState = false;
   
   function toggleModal(event: any) {
-    if (event.target.id === 'form') return;
+    if (event.target.id !== 'modal') return;
     modalState = !modalState;
     const modal = document.getElementById('modal');
     const form = document.getElementById('form');
     if (modal && form && modalState === true) {
       modal.style.display = "flex";
-      modal.animate([{opacity: 0}, {opacity: 0.9}], 
-        {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
-      form.animate([{transform: 'translateY(-200px)'}, {transform: 'translateY(0)'}], 
+      modal.animate([{backgroundColor: 'rgba(0, 0, 0, 0)'}, {backgroundColor: 'rgba(0, 0, 0, 0.6)'}], 
+          {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
+        form.animate([{opacity: 0}, {opacity: 1}], 
+          {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
+          form.animate([{transform: 'translateY(-200px)'}, {transform: 'translateY(0)'}], 
         {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
     }
     if (modal && form && modalState === false) {
-      modal.animate([{opacity: 0.9}, {opacity: 0}], 
+      modal.animate([{backgroundColor: 'rgba(0, 0, 0, 0.6)'}, {backgroundColor: 'rgba(0, 0, 0, 0)'}], 
+          {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
+      form.animate([{opacity: 1}, {opacity: 0}],
         {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
       form.animate([{transform: 'translateY(0)'}, {transform: 'translateY(-200px)'}], 
         {duration: 200, easing: 'ease-in-out', fill: 'forwards'});
@@ -35,10 +39,23 @@ const Modal: React.FC<propsInterface> = (props) => {
   return (
     <div>
       <div className='modal' id='modal' onClick={toggleModal}>
-        <div className='close'>+</div>
-        <div className='form' id='form'>form</div>
+        <div className='close' id='modal'>+</div>
+        <div className='form' id='form'>
+
+
+         <div className='title'> Join Worbli</div> 
+          <p className='text'>Worbli is the place to discover amazing dApps curated by our global community.</p>
+          <input type="input" className="input-text" name="email" placeholder="Email address"></input>
+          <input type="password" className="input-text" name="password" placeholder="Create a password"></input>          
+          <button className="btn-join">Join Worbli</button>
+          <p className='small-text'>Already on Worbli? <span>Log In.</span></p>
+     
+       
+
+
+        </div>
       </div>
-      <div onClick={toggleModal} className='modal-title'>{props.title}</div>
+      <div onClick={toggleModal} className='modal-title' id='modal'>{props.title}</div>
     </div>
   );
 }
